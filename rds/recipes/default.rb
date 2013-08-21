9e22d3450d1640e5540f8be293944b9f8bc51bb1
Tholkappiyan <tholkappiyan@Tholkappiyans-MacBook-Pro.local>
2013-08-21 11:47:11 +0530

    added cookbook rds

---
+++ rds/recipes/default.rb
@@ -0,0 +1,7 @@
+Chef::Log.info "1234567890"
+Chef::Log.info "~~~~~~~~~~"
+Chef::Log.info "~~~~~~~~~~"
+Chef::Log.info "~~~~~~~~~~"
+Chef::Log.info "~~~~~~~~~~"
+Chef::Log.info "~~~~~~~~~~"
+Chef::Log.info "1234567890"
