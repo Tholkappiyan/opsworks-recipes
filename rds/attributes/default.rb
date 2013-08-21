cd12996141e04e87a710dd767d0d2f6f42fc8a50
Tholkappiyan <tholkappiyan@Tholkappiyans-MacBook-Pro.local>
2013-08-21 15:36:20 +0530

    updated rds recipe

--- rds/attributes/default.rb
+++ rds/attributes/default.rb
@@ -1,9 +1,9 @@
 node[:deploy].each do |application, deploy|
-  default[:deploy][application][:database][:adapter] = "mysql"
-  default[:deploy][application][:database][:host] = "sampleapp-dbinstance.ci53wnwtchtk.us-east-1.rds.amazonaws.com"
-  default[:deploy][application][:database][:database_name] = "sample_app"
-  default[:deploy][application][:database][:user_name] = "sampleappuser"
-  default[:deploy][application][:database][:password] = "password"
-  default[:deploy][application][:database][:reconnect] = true
-  default[:deploy][application][:database][:port] = 3306
+  set[:deploy][application][:database][:adapter] = "mysql"
+  set[:deploy][application][:database][:host] = "sampleapp-dbinstance.ci53wnwtchtk.us-east-1.rds.amazonaws.com"
+  set[:deploy][application][:database][:database_name] = "sample_app"
+  set[:deploy][application][:database][:username] = "sampleappuser"
+  set[:deploy][application][:database][:password] = "password"
+  set[:deploy][application][:database][:reconnect] = true
+  set[:deploy][application][:database][:port] = 3306
 end
