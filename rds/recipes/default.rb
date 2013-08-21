f8fe7fac1e81b836b13b4d71be76d9cc41ea0549
Tholkappiyan <tholkappiyan@Tholkappiyans-MacBook-Pro.local>
2013-08-21 12:21:14 +0530

    adding database.yml file with test content

--- rds/recipes/default.rb
+++ rds/recipes/default.rb
@@ -5,3 +5,10 @@ Chef::Log.info "~~~~~~~~~~"
 Chef::Log.info "~~~~~~~~~~"
 Chef::Log.info "~~~~~~~~~~"
 Chef::Log.info "1234567890"
+
+template "/srv/www/simple_rails_app/shared/database.yml" do
+  source "database.yml.erb"
+  mode 0777
+  owner "root"
+  group "root"
+end
