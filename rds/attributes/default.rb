cbb785c7f38d6e31a68d10516dd294f7b35634db
Tholkappiyan <tholkappiyan@Tholkappiyans-MacBook-Pro.local>
2013-08-21 13:47:42 +0530

    updated rds attributes

--- rds/attributes/default.rb
+++ rds/attributes/default.rb
@@ -1,9 +1,9 @@
 node[:deploy].each do |application, deploy|
-  default[:deploy][application][:deploy][:database][:adapter] = "mysql"
-  default[:deploy][application][:deploy][:database][:host] = "localhost"
-  default[:deploy][application][:deploy][:database][:database] = "sample_app"
-  default[:deploy][application][:deploy][:database][:username] = "root"
-  default[:deploy][application][:deploy][:database][:password] = "root"
-  default[:deploy][application][:deploy][:database][:reconnect] = true
-  default[:deploy][application][:deploy][:database][:port] = 3306
+  default[:deploy][application][:database][:adapter] = "mysql"
+  default[:deploy][application][:database][:host] = "localhost"
+  default[:deploy][application][:database][:database] = "sample_app"
+  default[:deploy][application][:database][:username] = "root"
+  default[:deploy][application][:database][:password] = "root"
+  default[:deploy][application][:database][:reconnect] = true
+  default[:deploy][application][:database][:port] = 3306
 end
