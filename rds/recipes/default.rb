2bf9190929f013d5624fc48d49a06937456321f7
Tholkappiyan <tholkappiyan@Tholkappiyans-MacBook-Pro.local>
2013-08-21 12:52:06 +0530

    updated rds recipe

--- rds/recipes/default.rb
+++ rds/recipes/default.rb
@@ -8,7 +8,7 @@ Chef::Log.info "~~~~~~~~~~"
 Chef::Log.info "~~~~~~~~~~"
 Chef::Log.info "1234567890"
 
-  template "#{deploy[:deploy_to]}/shared/database.yml" do
+  template "/srv/www/simple_rails_app/shared/database.yml" do
     source "database.yml.erb"
     mode 0777
     owner "root"
