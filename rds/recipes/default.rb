af5d8bebe5e0131da51bc8d8a00a131b28f2e64c
Tholkappiyan <tholkappiyan@Tholkappiyans-MacBook-Pro.local>
2013-08-21 12:50:40 +0530

    update rds recipe

--- rds/recipes/default.rb
+++ rds/recipes/default.rb
@@ -8,13 +8,9 @@ Chef::Log.info "~~~~~~~~~~"
 Chef::Log.info "~~~~~~~~~~"
 Chef::Log.info "1234567890"
 
-node[:deploy].each do |application, deploy|
-  deploy = node[:deploy][application]
-  
   template "#{deploy[:deploy_to]}/shared/database.yml" do
     source "database.yml.erb"
     mode 0777
     owner "root"
     group "root"
   end
-end
