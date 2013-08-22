fff24504001eb6f907f4de3d50e59c98536e6b85
Tholkappiyan <tholkappiyan@Tholkappiyans-MacBook-Pro.local>
2013-08-22 12:18:34 +0530

    updated configure file in redis

--- redis/recipes/configure.rb
+++ redis/recipes/configure.rb
@@ -8,4 +8,9 @@ Chef::Log.info "1234567890"
 
 Chef::Log.info node[:opsworks][:layers][:redis][:instances][:redis1]
 
-Chef::Log.info node
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  Chef::Log.info deploy[:rails_env]
+
+end
