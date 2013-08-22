3ac133d08666e0dcfa814d554115d487009e5123
Tholkappiyan <tholkappiyan@Tholkappiyans-MacBook-Pro.local>
2013-08-22 14:51:15 +0530

    updated configure file in redis

--- redis/recipes/configure.rb
+++ redis/recipes/configure.rb
@@ -8,6 +8,27 @@ Chef::Log.info "1234567890"
 
 node[:deploy].each do |application, deploy|
 
+  execute "restart Rails app #{application}" do
+    cwd deploy[:current_path]
+    command node[:opsworks][:rails_stack][:restart_command]
+    action :nothing
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/redis.yml" do
+    source "redis.yml.erb"
+    mode 0777
+    owner "root"
+    group "root"
+    variables(:hostname => node[:opsworks][:layers][:redis][:instances][:redis1][:public_dns_name],
+    					:environment => node[:deploy][application][:rails_env])
+
+    notifies :run, resources(:execute => "restart Rails app #{application}")
+
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+
 	Chef::Log.info node[:opsworks][:layers][:redis][:instances][:redis1][:public_dns_name]
   Chef::Log.info node[:deploy][application][:rails_env]
 
